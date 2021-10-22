a6f8eeaeb8e33a658850100527e2e96139a128cc
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-10-22 09:39:08 -0400

    Cleanup

--- src/main/java/chronosacaria/testmod/blockentities/BreadBlockEntity.java
+++ src/main/java/chronosacaria/testmod/blockentities/BreadBlockEntity.java
@@ -3,7 +3,6 @@ package chronosacaria.testmod.blockentities;
 import chronosacaria.testmod.Testmod;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
 
 public class BreadBlockEntity extends BlockEntity {
